4f7b837338a2e1f5f6332e766cd78327d8b49525
Vaibhav Shukla <52421094+mr-vaibh@users.noreply.github.com>
2020-06-05 22:13:40 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+		integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<title>ReMART</title>
+
+	<style>
+		#searchTxt {
+			-webkit-transition: all 2s ease-in;
+			-moz-transition: all 2s ease-in;
+			transition: all 2s ease-in;
+		}
+
+		#searchTxt:focus {
+			width: 600px;
+		}
+	</style>
+</head>
+
+<body>
+
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		<a class="navbar-brand" href="#">ReMART</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+			aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav mr-auto">
+				<li class="nav-item active">
+					<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">About</a>
+				</li>
+			</ul>
+			<form class="form-inline my-2 my-lg-0">
+				<input class="form-control mr-sm-2" id="searchTxt" type="search" placeholder="Search"
+					aria-label="Search">
+			</form>
+		</div>
+	</nav>
+
+	<div class="container my-2">
+		<h1>Welcome to ReMART</h1>
+
+		<div class="card my-2">
+			<div class="card-body">
+				<div class="form-group">
+					<h5 class="card-title">Title</h5>
+					<input type="text" class="form-control" id="addTitle" aria-describedby="title" maxlength="25">
+				</div>
+
+				<h5 class="card-title">Note :</h5>
+				<div class="form-group">
+					<textarea class="form-control" name="text" id="addTxt" oninput="checkBtn();" rows="3"></textarea>
+				</div>
+				<button href="#" class="btn btn-primary" id="addBtn">Add Note</button>
+			</div>
+		</div>
+
+		<h3 class="my-2">Your Notes</h3>
+
+		<div class="row container-fluid" id="notes">
+
+			<!-- Demo Card
+			<div class="card border-dark shadow-lg mb-2 mx-2 noteCard" style="width: 18rem;">
+				<div class="card-header h5">title</div>
+				<div class="card-body text-dark">
+					<p class="card-text">note body</p>
+				</div>
+				<div class="card-footer text-muted">date <i class="fa fa-trash float-right" style="font-size: 24px;cursor: pointer;"></i> </div>
+			</div> -->
+
+		</div>
+	</div>
+
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+		integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+		crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+		integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+		crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+		integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+		crossorigin="anonymous"></script>
+	<script src="js/main.js"></script>
+</body>
+
+</html>
---
+++ js/main.js
@@ -0,0 +1,116 @@
+console.log('Welcome User !!');
+
+// Letting all necessary variables
+let addBtn = document.getElementById('addBtn');
+let addTxt = document.getElementById("addTxt");
+let addTitle = document.getElementById("addTitle");
+let searchTxt = document.getElementById("searchTxt");
+
+
+// Calling the necessary functions
+checkBtn();
+showNotes();
+
+// Function to check if textarea has input or not
+function checkBtn() {
+	if (addTxt.value == '') {
+		addBtn.disabled = true;
+		// console.log('disabled', addBtn.disabled);
+	} else {
+		addBtn.disabled = false;
+		// console.log('enabled', addBtn.disabled);
+	}
+}
+
+// If user adds a note, add it to the localStorage
+addBtn.addEventListener('click', function (e) {
+
+	let notes = localStorage.getItem("notes");
+
+	if (notes == null) {
+		notesObj = [];
+	} else {
+		notesObj = JSON.parse(notes);
+	}
+	let Obj = {
+		title: addTitle.value,
+		text: addTxt.value,
+		date: new Date().toLocaleDateString('en-GB'),
+		time: new Date().toLocaleTimeString(),
+	};
+	notesObj.push(Obj);
+	localStorage.setItem("notes", JSON.stringify(notesObj));
+	addTxt.value = "";
+	addTitle.value = "";
+	// console.log(notesObj);
+
+	showNotes();
+	checkBtn();
+});
+
+
+// Function to show notes from localStorage
+
+function showNotes() {
+	let notes = localStorage.getItem("notes");
+
+	if (notes == null) {
+		notesObj = [];
+	} else {
+		notesObj = JSON.parse(notes);
+	}
+
+	let html = '';
+	notesObj.forEach(function (element, index) {
+		html += `
+			<div class="card border-dark shadow-lg mb-2 mx-2 noteCard" style="width: 18rem;">
+				<div class="card-header h5" id="title${index}">${element.title}</div>
+				<div class="card-body text-dark">
+					<p class="card-text" id="text${index}">${element.text}</p>
+				</div>
+				<div class="card-footer text-muted">${element.date} at ${element.time}<i class="fa fa-trash float-right" title="Delete this note" id="${index}" onclick="deleteNote(this.id);" style="font-size: 24px;cursor: pointer;"></i> </div>
+			</div>
+		`;
+	});
+
+	let notesElm = document.getElementById('notes');
+	if (notesObj.length != 0) {
+		notesElm.innerHTML = html;
+	} else {
+		notesElm.innerHTML = `<p>No notes found, notes will be displayed here once added</p>`;
+	}
+}
+
+
+// Function to delete notes from localStorage
+
+function deleteNote(index) {
+	// console.log('Deleting notes', index);
+
+	let notes = localStorage.getItem("notes");
+	if (notes == null) {
+		notesObj = [];
+	} else {
+		notesObj = JSON.parse(notes);
+	}
+
+	notesObj.splice(index, 1);
+	localStorage.setItem("notes", JSON.stringify(notesObj));
+	showNotes();
+}
+
+
+searchTxt.addEventListener('input', function (e) {
+	let inputVal = searchTxt.value.toLowerCase();
+	let noteCards = document.getElementsByClassName('noteCard');
+	Array.from(noteCards).forEach(function (element) {
+		let cardTxt = element.getElementsByTagName('p')[0].innerText;
+		// console.log(cardTxt);
+		if (cardTxt.includes(inputVal)) {
+			element.style.display = 'inherit';
+		} else {
+			element.style.display = 'none';
+		}
+	})
+	
+})
